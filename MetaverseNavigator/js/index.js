086ac46ae64fd1bc9a9440f391e33f58a82ed9cd
HalfHydra <41239639+HalfHydra@users.noreply.github.com>
2019-05-12 22:28:51 -0400

    update

--- MetaverseNavigator/js/index.js
+++ MetaverseNavigator/js/index.js
@@ -3,3 +3,6 @@ $(".img-wrapper").mouseenter(function() {
     chain.currentTime = 0;
 		chain.play();
 });
+var music = document.getElementById('music');
+document.getElementById("music").loop = true;
+music.play();
