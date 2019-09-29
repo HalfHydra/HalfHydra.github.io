fe948d2b630ee92e54a64a2eca19eff3821fb1c2
Hydra <41239639+HalfHydra@users.noreply.github.com>
2019-09-29 00:27:14 -0400

    Metaverse Navigator Overhaul

--- MetaverseNavigator/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/HalfHydra/pen/pmgBWy.
-
- Re-creating the effect for persona 5's main menu screen for a tumblr theme.
--- MetaverseNavigator/js/index.js
+++
@@ -1,8 +0,0 @@
-var chain = $("#chains")[0];
-$(".img-wrapper").mouseenter(function() {
-    chain.currentTime = 0;
-		chain.play();
-});
-var music = document.getElementById('music');
-document.getElementById("music").loop = true;
-music.play();
