b825580c95ad04b2c710a4b7a9724806f9f93806
Hydra <41239639+HalfHydra@users.noreply.github.com>
2022-03-24 23:10:15 -0400

    Add tab system

--- Script/index.js
+++ Script/index.js
@@ -1,34 +1,75 @@
-let container = document.getElementById('container');
+let header_container = document.getElementById('header_container');
+
+let content_container = document.getElementById('container');
 
 let projects = {};
 
 let settings = {};
 
 window.addEventListener('load', () => {
-    let projects = {};
-    let settings = {};
     readTextFile("./Script/projects.json", 1);
     readTextFile("./Script/settings.json", 2);
 
     let headers = makeHeaders();
     headers.forEach(header => {
-        container.appendChild(header);
+        header_container.appendChild(header);
     })
+
+    let content = makeContent();
+    content.forEach(node => {
+        container.appendChild(node);
+    })
+
+    changeTab(settings.Default_Tab);
 });
 
 let makeHeaders = () => {
     let headers = [];
+
+    let name_header = document.createElement('div');
+    name_header.id = `HalfHydra`;
+    name_header.className = 'name_header_div';
+    name_header.addEventListener('click', () => {
+        //some sort of easter egg maybe
+    })
+    headers.push(name_header);
+
+    let name_header_img = document.createElement('img');
+    name_header_img.src = `./Images/Hydra.png`;
+    name_header_img.className = 'name_header_img';
+    name_header.appendChild(name_header_img);
+
+    let name_header_text = document.createElement('span');
+    name_header_text.innerHTML = `HalfHydra`;
+    name_header_text.className = 'name_header_text';
+    name_header.appendChild(name_header_text);
+
     settings.Tabs.forEach(tab => {
         let header = document.createElement('div');
         header.id = tab;
-        header.className = 'header';
+        header.className = 'header_tab';
+        header.addEventListener('click', () => {
+            changeTab(tab);
+        })
         headers.push(header);
 
         let header_text = document.createElement('span');
         header_text.innerHTML = tab;
+        header_text.className = 'header_text';
         header.appendChild(header_text);
     });
     return headers;
+}
+
+let makeContent = () => {
+    let content = [];
+    settings.Tabs.forEach(tab => {
+        let content_div = document.createElement('div');
+        content_div.id = `${tab}_content`;
+        content_div.className = 'content_div';
+        content.push(content_div);
+    });
+    return content;
 }
 
 let makeProjectModules = () => {
@@ -37,6 +78,16 @@ let makeProjectModules = () => {
     module.className = 'module';
     modules.push(module);
     return modules;
+}
+
+let changeTab = (tab) => {
+    document.getElementById('header_container').childNodes.forEach(header => {
+        if(header.id == `HalfHydra`) { return; }
+        (header.id === `${tab}`) ? header.className = 'header_tab_active' : header.className = 'header_tab';
+    });
+    document.getElementById('container').childNodes.forEach(container => {
+        (container.id === `${tab}_content`) ? container.className = 'content_div_active' : container.className = 'content_div';
+    });
 }
 
 async function readTextFile(file, a) {
