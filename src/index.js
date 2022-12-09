c98ebccfc576b719c5860959025148a77d722a53
Hydra <41239639+HalfHydra@users.noreply.github.com>
2022-12-09 04:39:36 -0500

    Completed overhaul of index page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-let header_container = document.getElementById('header_container');
-
 let content_container = document.getElementById('container');
 
 let projects = {};
 
 let settings = {};
+
+let content_text = {};
+
+let videos = {};
 
 window.mobileCheck = function() {
     let check = false;
@@ -26,11 +28,27 @@ window.addEventListener('load', () => {
         })
         .then(jsondata => {
             settings = jsondata;
+            return fetch("./src/content.json");
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(jsondata => {
+            content_text = jsondata;
+            return fetch("./src/videos.json");
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            videos = response;
             generatePage();
         })
 });
 
 let generatePage = () => {
+    let header_container = document.getElementById('header_container');
+
     let headers = makeHeaders();
     headers.forEach(header => {
         header_container.appendChild(header);
@@ -41,14 +59,116 @@ let generatePage = () => {
         container.appendChild(node);
     })
 
-    let archived_container = document.getElementById('Archived Projects_content');
-    let project_container = document.getElementById('Current Projects_content');
+    let project_container = document.getElementById('Projects_content');
 
     let projectModules = makeProjectModules();
     projectModules.forEach(project => {
-        console.log(project);
-        (project.getAttribute("archived") === "true") ? archived_container.appendChild(project) : project_container.appendChild(project);
+        project_container.appendChild(project)
     })
+
+    let video_container = document.getElementById('YouTube_content');
+
+    console.log(content_text);
+
+    let videoHeader = generateTextPanel(content_text["youtube_header"], "project_module", "youtube_header");
+    video_container.appendChild(videoHeader);
+
+    let videosDiv = document.createElement('div');
+    videosDiv.id = 'videosDiv';
+    videosDiv.className = 'videosDiv';
+    video_container.appendChild(videosDiv);
+
+    let videoModules = makeVideoModules();
+    videoModules.forEach(video => {
+        videosDiv.appendChild(video)
+    })
+
+    let about_container = document.getElementById('About_content');
+
+    let aboutLangauges = document.createElement('div');
+    aboutLangauges.id = 'aboutLangauges';
+    aboutLangauges.className = 'project_module';
+    about_container.appendChild(aboutLangauges);
+
+    let aboutLanguagesText = generateText(content_text["about_langauges_header"], "about_header");
+    aboutLangauges.appendChild(aboutLanguagesText);
+
+    let languagesDiv = document.createElement('div');
+    languagesDiv.className = 'languagesDiv';
+    aboutLangauges.appendChild(languagesDiv);
+
+    Object.keys(content_text.Languages).forEach(language => {
+        let languageObj = content_text.Languages[language];
+
+        let languageDiv = document.createElement('div');
+        languageDiv.className = 'languageDiv';
+        languagesDiv.appendChild(languageDiv);
+
+        let languageIconDiv = document.createElement('div');
+        languageIconDiv.className = 'languageIconDiv';
+        languageDiv.appendChild(languageIconDiv);
+
+        let languageIcon = document.createElement('i');
+        languageIcon.className = `${languageObj.Icon} colored languageIcon`;
+        languageIconDiv.appendChild(languageIcon);
+
+        let languageNameDiv = document.createElement('div');
+        languageNameDiv.className = 'languageTextDiv';
+        languageDiv.appendChild(languageNameDiv);
+
+        let languageNameText = document.createElement("p");
+        languageNameText.innerHTML = language;
+        languageNameDiv.appendChild(languageNameText);
+
+        let languageTimeDiv = document.createElement('div');
+        languageTimeDiv.className = 'languageTextDiv';
+        languageDiv.appendChild(languageTimeDiv);
+
+        let languageText = document.createElement("p");
+        languageTimeDiv.innerHTML = languageObj.Time;
+        languageTimeDiv.appendChild(languageText);
+    });
+
+    let aboutHeader = generateTextPanel(content_text["about_header"], "project_module", "about_header");
+    about_container.appendChild(aboutHeader);
+
+    let aboutBody = generateTextPanel(content_text["about_body"], "project_module", "about_body");
+    about_container.appendChild(aboutBody);
+
+    aboutBody.appendChild(generateText(content_text["about_body_2"], "about_body"))
+
+    let aboutBodyImg = document.createElement('img');
+    aboutBodyImg.src = content_text["img_about_1"];
+    aboutBodyImg.className = 'aboutBodyImg';
+    aboutBody.appendChild(aboutBodyImg);
+
+    aboutBody.appendChild(generateText(content_text["about_body_3"], "about_body"))
+
+    let aboutBodyImg2 = document.createElement('img');
+    aboutBodyImg2.src = content_text["img_about_2"];
+    aboutBodyImg2.className = 'aboutBodyImg';
+    aboutBody.appendChild(aboutBodyImg2);
+
+    aboutBody.appendChild(generateText(content_text["about_body_4"], "about_body"))
+
+    let aboutBodyImg3 = document.createElement('img');
+    aboutBodyImg3.src = content_text["img_about_3"];
+    aboutBodyImg3.className = 'aboutBodyImg';
+    aboutBody.appendChild(aboutBodyImg3);
+
+    aboutBody.appendChild(generateText(content_text["about_body_5"], "about_body"))
+
+    let aboutBodyImg4 = document.createElement('img');
+    aboutBodyImg4.src = content_text["img_about_4"];
+    aboutBodyImg4.className = 'aboutBodyImg';
+    aboutBody.appendChild(aboutBodyImg4);
+
+    aboutBody.appendChild(generateText(content_text["about_body_6"], "about_body"))
+
+    let aboutBodyImg5 = document.createElement('img');
+    aboutBodyImg5.src = content_text["img_about_5"];
+    aboutBodyImg5.className = 'aboutBodyImg';
+    aboutBody.appendChild(aboutBodyImg5);
 
     changeTab(settings.Default_Tab);
 }
@@ -74,10 +194,15 @@ let makeHeaders = () => {
     name_header_text.className = 'name_header_text';
     name_header.appendChild(name_header_text);
 
+    let isMobile = window.mobileCheck();
+
     settings.Tabs.forEach(tab => {
         let header = document.createElement('div');
         header.id = tab;
         header.className = 'header_tab';
+        if (isMobile) {
+            header.className = 'header_tab_mobile';
+        }
         header.addEventListener('click', () => {
             changeTab(tab);
         })
@@ -107,10 +232,11 @@ let makeProjectModules = () => {
     Object.keys(projects).forEach(project => {
         let projectObj = projects[project];
 
+        if(!projectObj.Active) { return }
+
         let module = document.createElement('div');
         module.className = 'project_module';
         module.id = project;
-        module.setAttribute("archived", projectObj.Archived)
 
         let module_div_1 = document.createElement('div');
         module_div_1.className = 'project_module_div_1';
@@ -130,13 +256,17 @@ let makeProjectModules = () => {
         module_text.className = 'project_module_text';
         module_div_2.appendChild(module_text);
 
+        if(projectObj.Archived) {
+            module_text.innerHTML = `[Archived] ${projectObj.Name}`;
+        }
+
         let module_desc = document.createElement('span');
         module_desc.innerHTML = projectObj.Description;
         module_desc.className = 'project_module_desc';
         module_div_2.appendChild(module_desc);
 
         let mobile = window.mobileCheck();
-        console.log(mobile);
+        //console.log(mobile);
 
         let module_div_3 = document.createElement('div');
         module_div_3.className = (mobile) ? 'project_module_div_3_mobile' : 'project_module_div_3';
@@ -171,6 +301,16 @@ let makeProjectModules = () => {
             module_div_3.appendChild(module_btn_view);
         }
 
+        if(projectObj.hasOwnProperty(`Video`)){
+            let module_btn_video = document.createElement('button');
+            module_btn_video.innerHTML = `Watch Demo`;
+            module_btn_video.className = 'module_btn_video';
+            module_btn_video.addEventListener('click', () => {
+                openTab(projectObj.Video);
+            })
+            module_div_3.appendChild(module_btn_video);
+        }
+
         let module_div_4 = document.createElement('div');
         module_div_4.id = `${project}_long_desc`;
         module_div_4.className = 'inactive';
@@ -181,16 +321,89 @@ let makeProjectModules = () => {
         module_long_desc.className = 'project_module_long_desc_text';
         module_div_4.appendChild(module_long_desc);
 
-
         modules.push(module);
     });
     return modules;
 }
 
+let makeVideoModules = () => {
+    let modules = [];
+    Object.keys(videos).forEach(video => {
+        let videoObj = videos[video];
+
+        if(!videoObj.Active) { return }
+
+        let module = document.createElement('div');
+        module.className = 'video_module';
+        module.id = video;
+
+        let module_div_1 = document.createElement('div');
+        module_div_1.className = 'video_module_div_1';
+        module.appendChild(module_div_1);
+
+        let module_text = document.createElement('span');
+        module_text.innerHTML = videoObj.title;
+        module_text.className = 'video_title_text';
+        module_div_1.appendChild(module_text);
+
+        let module_img = document.createElement('img');
+        module_img.src = videoObj.image;
+        module_img.className = 'video_module_img';
+        module.appendChild(module_img);
+
+        let module_div_2 = document.createElement('div');
+        module_div_2.className = 'video_module_div_2';
+        module.appendChild(module_div_2);
+        
+        let module_desc = document.createElement('span');
+        module_desc.innerHTML = videoObj.desc;
+        module_desc.className = 'video_module_desc';
+        module_div_2.appendChild(module_desc);
+        
+        let module_div_3 = document.createElement('div');
+        module_div_3.className = 'video_module_div_3';
+        module.appendChild(module_div_3);
+        
+        let module_btn_view = document.createElement('button');
+        module_btn_view.innerHTML = `Watch Video`;
+        module_btn_view.className = 'module_btn_view';
+        module_btn_view.addEventListener('click', () => {
+            openTab(videoObj.link);
+        })
+        module_div_3.appendChild(module_btn_view);
+        
+        modules.push(module);
+    });
+    return modules;
+}
+
+let generateTextPanel = (text, divClass, txtClass) => {
+    let textModule = document.createElement('div');
+    textModule.className = divClass;
+
+    let textModuleText = document.createElement('span');
+    textModuleText.innerHTML = text;
+    textModuleText.className = txtClass;
+    textModule.appendChild(textModuleText);
+
+    return textModule;
+}
+
+let generateText = (text, txtClass) => {
+    let textModuleText = document.createElement('span');
+    textModuleText.innerHTML = text;
+    textModuleText.className = txtClass;
+    return textModuleText;
+}
+
+
 let changeTab = (tab) => {
     document.getElementById('header_container').childNodes.forEach(header => {
         if(header.id == `HalfHydra`) { return; }
         (header.id === `${tab}`) ? header.className = 'header_tab_active' : header.className = 'header_tab';
+        if(window.mobileCheck()){
+            (header.id === `${tab}`) ? header.className = 'header_tab_active_mobile' : header.className = 'header_tab_mobile';
+        }
     });
     document.getElementById('container').childNodes.forEach(container => {
         (container.id === `${tab}_content`) ? container.className = 'content_div_active' : container.className = 'content_div';
@@ -204,3 +417,37 @@ let openTab = (url) => {
 let openInfo = (project) => {
     (document.getElementById(`${project}_long_desc`).className == 'inactive') ? document.getElementById(`${project}_long_desc`).className = 'project_module_long_desc' : document.getElementById(`${project}_long_desc`).className = 'inactive';
 }
+
+function ratioCalc(unknown, x1, x2, y1, y2){
+    switch(unknown){
+        case 0:
+            // x1/x2 == y1/y2
+            return x2 * (y1/y2)
+        case 1:
+            // x1/x2 == y1/y2
+            return x1 * (y2/y1)
+        case 2:
+            // x1/x2 == y1/y2
+            return y2 * (x1/x2)
+        case 3:
+            // x1/x2 == y1/y2
+            return y1 * (x2/x1)
+    }
+}
+
+window.addEventListener('resize', function(event) {
+    changeContentSize();
+}, true);
+
+function changeContentSize(){
+    //1920 1200 deviceWidth X
+    let browserScale = ratioCalc(3, 1920, 1200, window.innerWidth, window.innerHeight);
+    let mobile = window.mobileCheck();
+    if(!mobile && window.innerWidth > 1920){
+        $(document.getElementById("container")).css({ transform: `scale(${browserScale / 1200}, ${browserScale / 1200})`, transformOrigin: "top center" });
+    } else {
+        $(document.getElementById("container")).css({ transform: `scale(1, 1)`, transformOrigin: "top center" });
+    }
+}
+
+changeContentSize();
